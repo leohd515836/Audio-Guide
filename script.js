b35912b1c02868a8a72422fb62d9e5046e344d3b
LeoHD <55881995+leohd515836@users.noreply.github.com>
2023-03-17 15:48:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// 为列表项添加点击事件监听器
+document.querySelectorAll('li').forEach(item => {
+    item.addEventListener('click', () => {
+        // 获取列表项中的链接地址
+        const link = item.querySelector('a').getAttribute('href');
+        // 跳转到链接地址
+        window.location.href = link;
+    });
+});
+
+const button = document.getElementById('homepage');
+  
+// 添加点击事件监听器
+button.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
